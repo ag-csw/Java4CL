5ed5cb059bea2db459994f464019482e3882d2d0
ralph <csw@semantic-web.ro>
2014-12-02 18:36:37 +0100

    Setting up Maven project. Adding SLF4J library.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
-package main.java;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LOG.debug("Hello. This is the Java4CL package. The current time is {}.", System.currentTimeMillis());
 	}
 
 }
