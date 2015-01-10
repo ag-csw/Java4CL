c59a4b4161d93cb24aea6a0aa97b536da1fde3fa
greenTara <taraathan@gmail.com>
2015-01-10 16:54:52 -0500

    renaming to AllCLTests and deleting troublesome Structured classes

--- src/api4kb/main/java/krconfigured/StructuredKnowledgeEncodingConfigured.java
+++
@@ -1,9 +0,0 @@
-package krconfigured;
-
-import api4kbj.KnowledgeEncoding;
-import api4kbj.StructuredKnowledgeEncoding;
-
-public interface StructuredKnowledgeEncodingConfigured<T extends KnowledgeEncoding> extends
-		StructuredKnowledgeResourceConfigured<T>, StructuredKnowledgeEncoding<T> {
-
-}
--- src/api4kb/main/java/krconfigured/StructuredKnowledgeIOConfigured.java
+++
@@ -1,9 +0,0 @@
-package krconfigured;
-
-import api4kbj.KnowledgeIO;
-import api4kbj.StructuredKnowledgeIO;
-
-public interface StructuredKnowledgeIOConfigured extends KnowledgeIO,
-		StructuredKnowledgeResourceConfigured, StructuredKnowledgeIO {
-
-}
