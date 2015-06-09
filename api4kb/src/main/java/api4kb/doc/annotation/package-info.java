2b0825ff1d302eed4ed1ccb682452dfd9c6e2fd7
Ralph <schaef@inf.fu-berlin.de>
2015-06-09 16:10:19 +0200

    Create examples of package documentation.

---
+++ api4kb/src/main/java/api4kb/doc/annotation/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains custom annotation type declarations for documentation purposes.
+ *   
+ * @author ralph
+ */
+package api4kb.doc.annotation;
---
+++ api4kb/src/main/java/api4kb/doc/annotation/stability/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Annotation types in this package indicate source code stability. They share a common package, because there is no type system for annotations.
+ * @author ralph
+ */
+package api4kb.doc.annotation.stability;
