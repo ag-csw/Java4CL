8f30d7ec0ad39ca56e1ee2e077871aefa8d5bade
Ralph <schaef@inf.fu-berlin.de>
2015-06-09 16:09:42 +0200

    Create annotation types for levels of code stability.

---
+++ api4kb/src/main/java/api4kb/doc/annotation/stability/Alpha.java
@@ -0,0 +1,13 @@
+package api4kb.doc.annotation.stability;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Indicates that the annotated source code is in alpha state.
+ * @author ralph
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface Alpha {
+
+}
---
+++ api4kb/src/main/java/api4kb/doc/annotation/stability/Beta.java
@@ -0,0 +1,13 @@
+package api4kb.doc.annotation.stability;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Indicates that the annotated source code is in beta state.
+ * @author ralph
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface Beta {
+
+}
