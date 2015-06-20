457634b2335666b6846a99c7c9aa6cce66327d53
greenTara <taraathan@gmail.com>
2015-06-20 07:18:43 -0400

    fixing scalatest error

--- cl2/src/test/scala/TestAll.scala
+++ cl2/src/test/scala/TestAll.scala
@@ -6,7 +6,7 @@ class TestAll extends FlatSpec with Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
     val stack = new Stack[Int]
-    stack.push(3)
+    stack.push(1)
     stack.push(2)
     stack.pop() should be (2)
     stack.pop() should be (1)
