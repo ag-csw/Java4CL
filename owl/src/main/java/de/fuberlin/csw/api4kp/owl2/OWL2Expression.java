ea9ae3bff85a0476fb17b9ebbbe924ad3d6f36cb
Ralph <schaef@inf.fu-berlin.de>
2015-08-12 14:44:51 +0200

    Add owl subproject stub.

---
+++ owl/src/main/java/de/fuberlin/csw/api4kp/owl2/OWL2Expression.java
@@ -0,0 +1,16 @@
+package de.fuberlin.csw.api4kp.owl2;
+
+import org.semanticweb.owlapi.model.OWLObject;
+
+import api4kbj.BasicKnowledgeExpression;
+
+public class OWL2Expression {//implements OWL2KnowledgeResource, BasicKnowledgeExpression, OWL2Commentable {
+	
+	private OWLObject target;
+	
+	public OWLObject getTarget() {
+		return target;
+	}
+	
+	
+}
---
+++ owl/src/main/java/de/fuberlin/csw/api4kp/owl2/OWLEntity.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package de.fuberlin.csw.api4kp.owl2;
+
+/**
+ * @author ralph
+ *
+ */
+public class OWLEntity {
+
+}
