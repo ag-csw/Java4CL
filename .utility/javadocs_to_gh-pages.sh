c1ebcb5843b4c98928b76187ed2095159b0216aa
RalphBln <RalphBln@users.noreply.github.com>
2015-08-14 18:51:31 +0200

    let javadocs_to_gh-pages.sh do meaningful things

--- .utility/javadocs_to_gh-pages.sh
+++ .utility/javadocs_to_gh-pages.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-echo 'TRAVIS_REPO_SLUG='$TRAVIS_REPO_SLUG
-echo 'TRAVIS_JDK_VERSION='$TRAVIS_JDK_VERSION
-echo 'TRAVIS_PULL_REQUEST='$TRAVIS_PULL_REQUEST
-echo 'TRAVIS_BRANCH='$TRAVIS_BRANCH
-echo 'HOME='$HOME
-echo 'GH_TOKEN='${GH_TOKEN}
-echo 'TRAVIS_BUILD_NUMBER='$TRAVIS_BUILD_NUMBER
+# adapted from https://github.com/ReadyTalk/swt-bling/blob/master/.utility/push-javadoc-to-gh-pages.sh
+# with the aid of http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+# Thanks to Ben Limmer
+
+if [ "$TRAVIS_REPO_SLUG" == "ag-csw/Java4CL" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "java8" ]; then
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R build/docs/javadoc $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ag-csw/Java4CL gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
+  
+fi
