738d5c5bb3470503cc33ee5532d07cbf78d272e3
greenTara <taraathan@gmail.com>
2015-08-18 09:58:18 -0400

    adding outline for non-String comment class

---
+++ cl2/src/main/java/cl2/CLImageComment.java
@@ -0,0 +1,19 @@
+package cl2;
+
+import java.awt.Image;
+
+import cl2a.CLComment;
+
+public class CLImageComment extends CLComment  {
+
+
+	public CLImageComment(Image data) {
+		super(data);
+	}
+
+	@Override
+	public Image data() {
+		return (Image) super.data();
+	}
+
+}
